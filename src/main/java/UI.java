b38b0eeb497e8e028ebe33da975d4d2bf173d6ca
Maloulou99 <baha0610@stud.kea.dk>
2022-09-26 17:37:32 +0200

    Aflevering 1

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
         }
 
         public int getScanInt() {
-            return this.getScanInt("Indtastet ikke tilladt, skal indtastes et tal: ");
+            return this.getScanInt("Value is not allowed, try again ");
         }
 
         public String getScanString() {
@@ -25,73 +25,75 @@ import java.util.Scanner;
         // Reset
         public static final String RESET = "\033[0m";  // Text Reset
 
-        // Tekst i farver
-        public static final String sort = "\033[0;30m";   // BLACK
-        public static final String rød = "\033[0;31m";     // RED
-        public static final String grøn = "\033[0;32m";   // GREEN
-        public static final String gul = "\033[0;33m";  // YELLOW
-        public static final String blå = "\033[0;34m";    // BLUE
-        public static final String lilla = "\033[0;35m";  // PURPLE
-        public static final String turkisblå = "\033[0;36m";    // CYAN
-        public static final String hvid = "\033[0;37m";   // WHITE
+        // Regular Colors
+        public static final String BLACK = "\033[0;30m";   // BLACK
+        public static final String RED = "\033[0;31m";     // RED
+        public static final String GREEN = "\033[0;32m";   // GREEN
+        public static final String YELLOW = "\033[0;33m";  // YELLOW
+        public static final String BLUE = "\033[0;34m";    // BLUE
+        public static final String PURPLE = "\033[0;35m";  // PURPLE
+        public static final String CYAN = "\033[0;36m";    // CYAN
+        public static final String WHITE = "\033[0;37m";   // WHITE
 
-        // Fremhævet farve
-        public static final String sort_FH = "\033[1;30m";  // BLACK
-        public static final String rød_FH = "\033[1;31m";    // RED
-        public static final String grøn_FH = "\033[1;32m";  // GREEN
-        public static final String gul_FH = "\033[1;33m"; // YELLOW
-        public static final String blå_FH = "\033[1;34m";   // BLUE
-        public static final String lilla_FH = "\033[1;35m"; // PURPLE
-        public static final String turkisblå_FH = "\033[1;36m";   // CYAN
-        public static final String hvid_FH = "\033[1;37m";  // WHITE
+        // Bold
+        public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+        public static final String RED_BOLD = "\033[1;31m";    // RED
+        public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+        public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+        public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+        public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+        public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+        public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
 
-        // Understreg
-        public static final String sort_understreg = "\033[4;30m";  // BLACK
-        public static final String rød_understreg = "\033[4;31m";    // RED
-        public static final String grøn_understreg = "\033[4;32m";  // GREEN
-        public static final String gul_understreg = "\033[4;33m"; // YELLOW
-        public static final String blå_understreg = "\033[4;34m";   // BLUE
-        public static final String lilla_understreg = "\033[4;35m"; // PURPLE
-        public static final String turkisblå_understreg = "\033[4;36m";   // CYAN
-        public static final String hvid_understreg = "\033[4;37m";  // WHITE
+        // Underline
+        public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
+        public static final String RED_UNDERLINED = "\033[4;31m";    // RED
+        public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
+        public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+        public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
+        public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+        public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
+        public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
 
-        // Baggrund
-        public static final String sort_Baggrund = "\033[40m";  // BLACK
-        public static final String rød_Baggrund = "\033[41m";    // RED
-        public static final String grøn_Baggrund  = "\033[42m";  // GREEN
-        public static final String gul_Baggrund = "\033[43m"; // YELLOW
-        public static final String blå_Baggrund  = "\033[44m";   // BLUE
-        public static final String lilla_Baggrund = "\033[45m"; // PURPLE
-        public static final String turkisblå_Baggrund = "\033[46m";   // CYAN
-        public static final String hvid_Baggrund  = "\033[47m";  // WHITE
+        // Background
+        public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
+        public static final String RED_BACKGROUND = "\033[41m";    // RED
+        public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
+        public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+        public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
+        public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+        public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
+        public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
 
-        // Høj lysintensitet
-        public static final String sort_HøjLys = "\033[0;93m"; // YELLOW
-        public static final String blå_HøjLys = "\033[0;94m";   // BLUE
-        public static final String lilla_HøjLys = "\033[0;95m"; // PURPLE
-        public static final String turkisblå_HøjLys = "\033[0;96m";   // CYAN
-        public static final String hvid_HøjLys = "\033[0;97m";  // WHITE
+        // High Intensity
+        public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+        public static final String RED_BRIGHT = "\033[0;91m";    // RED
+        public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+        public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+        public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+        public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+        public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+        public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
 
-        // Høj tekst fremhævning
-        public static final String sort_Tekst_FM = "\033[1;90m"; // BLACK
-        public static final String rød_Tekst_FM = "\033[1;91m";   // RED
-        public static final String grøn_Tekst_FM = "\033[1;92m"; // GREEN
-        public static final String gul_Tekst_FM = "\033[1;93m";// YELLOW
-        public static final String blå_Tekst_FM = "\033[1;94m";  // BLUE
-        public static final String lilla_Tekst_FM = "\033[1;95m";// PURPLE
-        public static final String turkisblå_Tekst_FM = "\033[1;96m";  // CYAN
-        public static final String hvid_Tekst_FM = "\033[1;97m"; // WHITE
+        // Bold High Intensity
+        public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+        public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
+        public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+        public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
+        public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
+        public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+        public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
+        public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
 
-        // Høj baggrunds fremhævning
-        public static final String sort_FM_baggrund = "\033[0;100m";// BLACK
-        public static final String rød_FM_baggrund = "\033[0;101m";// RED
-        public static final String grøn_FM_baggrund = "\033[0;102m";// GREEN
-        public static final String gul_FM_baggrund = "\033[0;103m";// YELLOW
-        public static final String blå_FM_baggrund = "\033[0;104m";// BLUE
-        public static final String lilla_FM_baggrund = "\033[0;105m"; // PURPLE
-        public static final String turkisblå_FM_baggrund = "\033[0;106m";  // CYAN
-        public static final String hvid_FM_baggrund = "\033[0;107m";   // WHITE
-
+        // High Intensity backgrounds
+        public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+        public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+        public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+        public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+        public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
+        public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
+        public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
+        public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
 
     }
 
--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 
 public class Userinterface {
     private Adventure adventure;
-    private UI ui;
-    private Scanner sc = new Scanner(System.in);
+    private UI ui = new UI();
+    String input;
 
     public void startProgram() {
-        System.out.println("Hi, Welcome to the adventure game let's play!");
-        command();
-    }
+        System.out.println("""
+                Hi, Welcome to the adventure game let's play!
+                1. North
+                2. East
+                3. South
+                4. West
+                5. Look
+                9. End Game""");
 
-    private void command() {
-        System.out.println("Select an option ");
-        String input;
-        input = ui.getScanString();
+
+        input = ui.getScanString(); //String som giver errormessage, hvis bruger skriver en anden karakter
+        command(input);
+    }
+        private void command(String input) {
+            System.out.println("Select an option ");
 
         switch (input) {
             case "North":
