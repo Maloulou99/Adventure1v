9ceab61a7d15c5284014277bb84d26f0fee5a974
Maloulou99 <baha0610@stud.kea.dk>
2022-09-27 13:23:30 +0200

    Option ændret

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -16,37 +16,39 @@ public class Adventure {
 
 
     public void mapGame() {
-        room1 = new Room("Room 1", "The first room.");
-        room2 = new Room("Room 2", "The second room");
-        room3 = new Room("Room 3", "The third room");
-        room4 = new Room("Room 4", "The fourth room");
-        room5 = new Room("Room 5", "The fifth room");
-        room6 = new Room("Room 4", "The sixth room");
-        room7 = new Room("Room 4", "The seventh room");
-        room8 = new Room("Room 4", "The eighth room");
-        room9 = new Room("Room 4", "The ninth room");
+        room1 = new Room("Room 1", "room with no distinct features, except two doors");
+        room2 = new Room("Room 2", "room that has a lot things lying around, only one other door");
+        room3 = new Room("Room 3", "room with a lot a skeletons and an old axe, and only one other door");
+        room4 = new Room("Room 4", "room with a giant ready to kill you, fight or run too the door");
+        room5 = new Room("Room 5", "room with a bag of gold lying on the ground, there are no other doors to go through");
+        room6 = new Room("Room 6", "room that has a shield hanging at the wall, take it or proceed to the next room");
+        room7 = new Room("Room 7", "room that has pictures of Kings and Queens, only one door\"");
+        room8 = new Room("Room 8", "room with two doors, what could they lead too?");
+        room9 = new Room("Room 9", "room with a minotaurus that are the strongest creature, fight it or flee too the door");
 
-        //TODO der skal være fire attributer, men er den sådan??
-        room1.setRooms(room2, null, null, null);
+
+
+        //TODO kig billede igennem og ret
+        room1.setRooms(room2, room4, null, null);
         room2.setRooms(room1, room3, null, null);
         room3.setRooms(room2, room4, null, null);
-        room4.setRooms(room3, room5, null, null);
-        room5.setRooms(room3, room5, null, null);
-        room6.setRooms(room5, room7, null, null);
-        room7.setRooms(room6, room8, null, null);
-        room8.setRooms(room7, room9, null, null);
-        room9.setRooms(room8, null, null, null);
+        room4.setRooms(room1, room7, null, null);
+        room5.setRooms(room8, null, null, null);
+        room6.setRooms(room3, room9, null, null);
+        room7.setRooms(room4, room8, null, null);
+        room8.setRooms(room5, room7, room9, null);
+        room9.setRooms(room6, room8, null, null);
 
 
         //TODO ændre til uden hardcode....
-        /*this.currentRoom = room1;
-        this.currentRoom = room3;
+        this.currentRoom = room1;
+        /*this.currentRoom = room3;
         this.currentRoom = room4;
         this.currentRoom = room5;
         this.currentRoom = room6;
         this.currentRoom = room7;
-        this.currentRoom = room8;*/
-        this.currentRoom = room9;
+        this.currentRoom = room8;
+        this.currentRoom = room9;*/
     }
 
     //Tilføj rummene med output til bruger
