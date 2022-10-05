6fc3a71925ecb7486daf6e2aad1d43512d308ad1
Maloulou99 <baha0610@stud.kea.dk>
2022-10-05 19:43:56 +0200

    Aflevering 3

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -13,45 +13,42 @@ public class Map {
         Room room9 = new Room("Room 9", "room with a chair");
 
 
-
-        //TODO skal tilføje kan eat eller ikke
-        //TODO skil food og item fra
         room1.setRoomEast(room2);
         room1.setRoomSouth(room4);
 
         room2.setRoomWest(room1);
         room2.setRoomEast(room3);
         room2.addItem("lamp", " an old lamp hanging at the wall");
-        room2.addFood("", " this lamp are to nasty, and not edible", 0);
+        room2.addFood("banana", " looks ok, let me take it", 10);
 
         room3.setRoomWest(room2);
         room3.setRoomSouth(room6);
-        room3.addItem("pizza", " a pizza that seems to have go gains");
-        room3.addFood("pizza", " oh, who ordered pizza? It's to delicious", 45 );
+        room3.addItem("blanket", " lies curled up on the floor");
+        room3.addFood("pizza", " oh, who ordered pizza? It's to delicious", 35 );
 
         room4.setRoomNorth(room1);
         room4.setRoomSouth(room7);
         room4.addItem("sword", " a shiny sword is laying on the ground");
-        room4.addFood("sword", " you are kind of stupid, it's too sharp", 0);
+        room4.addFood("chili", " chili there wake you up a slightly ", 12);
 
         room5.setRoomSouth(room8);
-        room5.addItem("apple", " a big bag of appels");
+        room5.addItem("tv", " can you even watch tv on that tv?");
         room5.addFood("apple", " the apple is poisoned", -34);
 
         room6.setRoomNorth(room3);
         room6.setRoomSouth(room9);
         room6.addItem("map", " an old map, seems like it could lead to something valuable");
-        room6.addFood("map", " you cannot eat the map", 0);
+        room6.addFood("Spaghetti", " too much cheese", -23);
 
         room7.setRoomNorth(room4);
         room7.setRoomEast(room8);
         room7.addItem("torch", " a torch hanging at the wall, that could help light up dark places");
-        room7.addFood("torch", " you will burn, don't eat that", 0);
+        room7.addFood("sandwich", " there is a fly in the food, eww ", -26);
 
         room8.setRoomWest(room7);
         room8.setRoomEast(room9);
         room8.addItem("shield", " a shield that seems to have a royal crest on it");
-        room8.addFood("shield", " too hard to thicken, cannot eat the item", 0);
+        room8.addFood("proteincookie", " seems to have good gains", 45);
 
         room9.setRoomNorth(room6);
         room9.setRoomWest(room8);
